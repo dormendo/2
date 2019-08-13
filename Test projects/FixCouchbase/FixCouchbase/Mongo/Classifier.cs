89838bec9b64cdfd2840252642e1806e8c855ddf
Alexander Feoktistov <slavarossie@hotmail.com>
2019-08-13 14:40:35 +0300

    Тесты индексов MongoDb

---
+++ Test projects/FixCouchbase/FixCouchbase/Mongo/Classifier.cs
@@ -0,0 +1,181 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixCouchbase
+{
+	public class Classifier
+	{
+		public enum Type : byte
+		{
+			CProps = 1,
+			CElements = 2,
+			CLinks = 3,
+			EProps = 4,
+			EExtCols = 5,
+			ELinks = 6,
+			EAncLinks = 7
+		}
+
+		public string Key { get; private set; }
+
+		public Type CacheType { get; private set; }
+
+		public int ClassifierId { get; private set; }
+
+		public int? ElementId { get; private set; }
+
+		public Guid? ElementGuid { get; private set; }
+
+		public byte[] Value { get; private set; }
+
+		public Classifier(Type cacheType, int classifierId)
+			: this(null, cacheType, classifierId, null, null, null)
+		{
+		}
+
+		public Classifier(Type cacheType, int classifierId, int elementId, Guid elementGuid)
+			: this(null, cacheType, classifierId, elementId, elementGuid, null)
+		{
+		}
+
+		private unsafe Classifier(string key, Type cacheType, int classifierId, int? elementId, Guid? elementGuid, byte[] value)
+		{
+			this.CacheType = cacheType;
+			this.ClassifierId = classifierId;
+			this.ElementId = elementId;
+			this.ElementGuid = elementGuid;
+			this.Key = (key ?? this.GetKey());
+
+			if (value != null)
+			{
+				this.Value = value;
+			}
+			else
+			{
+				this.Value = new byte[32 * 1024];
+				fixed (byte* bp = this.Value)
+				{
+					int* ip = (int*)bp;
+					for (int i = 0; i < 32 * 1024 / 4; i++)
+					{
+						ip[i] = i;
+					}
+				}
+			}
+		}
+
+		private string GetKey()
+		{
+			switch (this.CacheType)
+			{
+				case Type.CProps:
+					return "cp" + this.ClassifierId.ToString();
+				case Type.CElements:
+					return "ce" + this.ClassifierId.ToString();
+				case Type.CLinks:
+					return "cl" + this.ClassifierId.ToString();
+				case Type.EProps:
+					return "ep" + this.ElementId.ToString();
+				case Type.EExtCols:
+					return "ec" + this.ElementId.ToString();
+				case Type.ELinks:
+					return "el" + this.ElementId.ToString();
+				default:
+					return "ea" + this.ElementId.ToString();
+			}
+		}
+
+		public BsonDocument ToBsonDocument()
+		{
+			if (this.ElementId.HasValue)
+			{
+				return new BsonDocument {
+					{ "_id", this.Key },
+					{ "t", (int)this.CacheType },
+					{ "c", this.ClassifierId },
+					{ "e", this.ElementId.Value },
+					{ "eg", new BsonBinaryData(this.ElementGuid.Value) },
+					{ "v", new BsonBinaryData(this.Value) }
+				};
+			}
+			else
+			{
+				return new BsonDocument {
+					{ "_id", this.Key },
+					{ "t", (int)this.CacheType },
+					{ "c", this.ClassifierId },
+					{ "v", new BsonBinaryData(this.Value) }
+				};
+			}
+		}
+
+		public static Classifier FromBsonDocument(BsonDocument bd)
+		{
+			string key = bd.GetValue("_id").AsString;
+			Type type = (Type)bd.GetValue("t").AsInt32;
+			int classifierId = bd.GetValue("c").AsInt32;
+			int? elementId = null;
+			Guid? elementGuid = null;
+			if (type == Type.EAncLinks || type == Type.EExtCols || type == Type.ELinks || type == Type.EProps)
+			{
+				elementId = bd.GetValue("e").AsInt32;
+				elementGuid = bd.GetValue("eg").AsGuid;
+			}
+
+			byte[] cache = bd.GetValue("v")?.AsBsonBinaryData?.Bytes;
+
+			return new Classifier(key, type, classifierId, elementId, elementGuid, cache);
+		}
+
+		internal BsonDocument GetFindByIdBson()
+		{
+		 	return new BsonDocument { { "_id", this.Key } };
+		}
+
+		internal BsonDocument GetFindByClassifierIdBson()
+		{
+			return new BsonDocument { { "c", this.ClassifierId }, { "t", (int)this.CacheType } };
+		}
+
+		internal bool Compare(Classifier c1)
+		{
+			if (c1 == null || c1.Value == null)
+			{
+				return false;
+			}
+
+			if (c1.CacheType != this.CacheType || c1.ClassifierId != this.ClassifierId || c1.ElementId != this.ElementId || c1.ElementGuid != this.ElementGuid)
+			{
+				return false;
+			}
+
+			return CompareBuffers(this.Value, c1.Value);
+		}
+
+		private static unsafe bool CompareBuffers(byte[] b1, byte[] b2)
+		{
+			if (b1.Length != b2.Length)
+			{
+				return false;
+			}
+
+			fixed (byte* bp1 = b1, bp2 = b2)
+			{
+				for (int i = 0; i < b1.Length; i++)
+				{
+					if (bp1[i] != bp2[i])
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
--- Test projects/FixCouchbase/FixCouchbase/Mongo/Mongo.cs
+++ Test projects/FixCouchbase/FixCouchbase/Mongo/Mongo.cs
@@ -16,16 +16,22 @@ namespace FixCouchbase
 			PrepareMongo();
 
 			// 1. Одновременное сохранение по 10 записей.
-			_metadata.DeleteMany(new BsonDocument());
-			TestMongo1(200, 10, 2);
-			TestMongo2(2000, 2);
-
-			_metadata.DeleteMany(new BsonDocument());
-			TestMongo1(20, 100, 5);
-			TestMongo2(2000, 5);
 			// 2. Чтение по одной записи с поиском по ключу
-			// 3. Одновременное сохранение по 10 записей. У записи два индекса - идентификатор классификатора и идентификатор раздела.
+			_metadata.DeleteMany(new BsonDocument());
+			//TestMongo1(200, 10, 2);
+			//TestMongo2(2000, 2);
+
+			//_metadata.DeleteMany(new BsonDocument());
+			//TestMongo1(20, 100, 5);
+			//TestMongo2(2000, 4);
+
+
+
+			// 3. Одновременное сохранение по N записей. У записи два индекса - идентификатор классификатора и идентификатор раздела.
+			_classifier.DeleteMany(new BsonDocument());
+			TestMongo3(10000);
 			// 4. Чтение по одной записи с поиском по индексу и типу записи.
+			TestMongo4(10000);
 			// 5. Удаление группы записей по индексу.
 			// 6. Массовое сохранение записей в коллекцию с одним индексом по "ElementId, ФИО"
 			// 7. Переименование ключа из п.6.
@@ -34,11 +40,19 @@ namespace FixCouchbase
 		private MongoClient _mc;
 		private IMongoDatabase _db;
 		private IMongoCollection<BsonDocument> _metadata;
+		private IMongoCollection<BsonDocument> _classifier;
+
+		private Classifier.Type[] classifierTypes = {
+			Classifier.Type.CProps, Classifier.Type.CElements, Classifier.Type.CLinks,
+			Classifier.Type.EProps, Classifier.Type.EExtCols, Classifier.Type.ELinks, Classifier.Type.EAncLinks
+		};
+
 		private void PrepareMongo()
 		{
-			_mc = new MongoClient("mongodb://nsicluster1:27017");
+			_mc = new MongoClient("mongodb://localhost:27017");
 			_db = _mc.GetDatabase("Norma");
 			_metadata = _db.GetCollection<BsonDocument>("Metadata");
+			_classifier = _db.GetCollection<BsonDocument>("Classifier");
 		}
 
 		private void TestMongo1(int iterations, int itemsPerIteration, int offset)
@@ -109,5 +123,121 @@ namespace FixCouchbase
 			sw.Stop();
 			Console.WriteLine($"TestMongo2. {sw.Elapsed.ToString()}, {sw.ElapsedMilliseconds * 1000 / count} / 1000, {nulls}, {incorrect}");
 		}
+
+
+		private void TestMongo3(int iterations)
+		{
+			List<WriteModel<BsonDocument>> list = new List<WriteModel<BsonDocument>>(7);
+
+			Stopwatch sw = new Stopwatch();
+			for (int i = 0; i < iterations; i++)
+			{
+				string iStr = i.ToString();
+				Guid elementGuid = new Guid("00000000-0000-0000-0000-" + new string('0', 12 - iStr.Length) + iStr);
+				list.Clear();
+				for (int j = 0; j < classifierTypes.Length; j++)
+				{
+					Classifier.Type type = classifierTypes[j];
+					Classifier c;
+					if (type == Classifier.Type.CProps || type == Classifier.Type.CElements || type == Classifier.Type.CLinks)
+					{
+						c = new Classifier(type, i);
+					}
+					else
+					{
+						c = new Classifier(type, i, i, elementGuid);
+					}
+
+					BsonDocument mdbd = c.ToBsonDocument();
+					list.Add(new ReplaceOneModel<BsonDocument>(new BsonDocument("_id", c.Key), mdbd) { IsUpsert = true });
+				}
+
+				sw.Start();
+				_classifier.BulkWrite(list);
+				sw.Stop();
+
+				if (((i + 1) % 100) == 0)
+				{
+					Console.WriteLine(i + 1);
+				}
+			}
+
+			sw.Stop();
+			Console.WriteLine($"TestMongo3. {iterations} итераций. {sw.Elapsed.ToString()}, {sw.ElapsedMilliseconds * 1000 / iterations} мc/1000");
+		}
+
+		private void TestMongo4(int count)
+		{
+			int nulls1 = 0, nulls2 = 0;
+			int incorrect1 = 0, incorrect2 = 0;
+			Stopwatch sw1 = new Stopwatch();
+			Stopwatch sw2 = new Stopwatch();
+			for (int i = 0; i < count; i++)
+			{
+				string iStr = i.ToString();
+				Guid elementGuid = new Guid("00000000-0000-0000-0000-" + new string('0', 12 - iStr.Length) + iStr);
+
+				for (int j = 0; j < classifierTypes.Length; j++)
+				{
+					Classifier.Type type = classifierTypes[j];
+					Classifier c;
+					if (type == Classifier.Type.CProps || type == Classifier.Type.CElements || type == Classifier.Type.CLinks)
+					{
+						c = new Classifier(type, i);
+					}
+					else
+					{
+						c = new Classifier(type, i, i, elementGuid);
+					}
+
+					BsonDocument byIdBd = c.GetFindByIdBson();
+					BsonDocument byIdxBd = c.GetFindByClassifierIdBson();
+
+					sw1.Start();
+					BsonDocument bd1 = _classifier.Find(byIdBd).FirstOrDefault();
+					sw1.Stop();
+
+					if (bd1 == null)
+					{
+						nulls1++;
+					}
+					else
+					{
+						Classifier c1 = Classifier.FromBsonDocument(bd1);
+						if (!c.Compare(c1))
+						{
+							incorrect1++;
+						}
+					}
+
+					sw2.Start();
+					BsonDocument bd2 = _classifier.Find(byIdxBd).FirstOrDefault();
+					sw2.Stop();
+
+					if (bd2 == null)
+					{
+						nulls2++;
+					}
+					else
+					{
+						Classifier c2 = Classifier.FromBsonDocument(bd2);
+						if (!c.Compare(c2))
+						{
+							incorrect2++;
+						}
+					}
+
+				}
+
+				if (((i + 1) % 100) == 0)
+				{
+					Console.WriteLine(i + 1);
+				}
+			}
+
+			Console.WriteLine($"TestMongo4. Id:{sw1.Elapsed.ToString()}, {sw1.ElapsedMilliseconds * 1000 / count} / 1000, {nulls1}, {incorrect1}");
+			Console.WriteLine($"TestMongo4. Idx:{sw2.Elapsed.ToString()}, {sw2.ElapsedMilliseconds * 1000 / count} / 1000, {nulls2}, {incorrect2}");
+		}
+
 	}
 }
